448e0f06da5c094ef4b07b60126aa05dfa7b06a9
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-01-02 03:44:40 +0300

    Category Add Destroy

--- resources/views/admin/_sidebar.blade.php
+++ resources/views/admin/_sidebar.blade.php
@@ -17,7 +17,7 @@
             </li>
             <li class="nav-item has-treeview">
                 <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-copy"></i>
+                    <i class="material-icons"></i>
                     <p>
                         Products
                         <i class="fas fa-angle-left right"></i>
