604e7f78f6ffb6a9121014ac1470d22c9ab6654f
Efekan Gündüz <2015010206048@ogrenci.karabuk.edu.tr>
2021-01-02 03:44:40 +0300

    Category Add Destroy

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -22,15 +22,35 @@ class CategoryController extends Controller
 
         return view('admin.category', ['datalist' => $datalist]);
     }
-
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function add()
     {
-        //
+        $datalist = DB::table('categories')->get()->where('parent_id',0);
+       // print_r($datalist);
+       // exit();
+        return view('admin.category_add', ['datalist' => $datalist]);
+    }
+    /**
+     * Insert data
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+
+        DB::table('categories')->insert([
+            'parent_id'=>$request->input('parent_id'),
+            'title'=>$request->input('title'),
+            'keywords'=>$request->input('keywords'),
+            'description'=>$request->input('description'),
+            'slug'=>$request->input('slug'),
+            'status'=>$request->input('status')
+        ]);
+        return redirect()->route('admin_category');
     }
 
     /**
@@ -41,7 +61,7 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
     }
 
     /**
@@ -61,9 +81,11 @@ class CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function edit(Category $category)
+    public function edit(Category $category,$id)
     {
-        //
+        $data= Category::find($id);
+        $datalist = DB::table('categories')->get()->where('parent_id',0);
+        return view('admin.category_edit', ['data'=>$data,'datalist'=>$datalist]);
     }
 
     /**
@@ -73,9 +95,17 @@ class CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Category $category)
+    public function update(Request $request, Category $category,$id)
     {
-        //
+            $data = Category::find($id);
+            $data->parent_id=$request->input('parent_id');
+            $data->title=$request->input('title');
+            $data->keywords=$request->input('keywords');
+            $data->description=$request->input('description');
+            $data-> slug=$request->input('slug');
+            $data->status=$request->input('status');
+            $data->save();
+        return redirect()->route('admin_category');
     }
 
     /**
@@ -84,8 +114,9 @@ class CategoryController extends Controller
      * @param  \App\Models\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Category $category)
+    public function destroy(Category $category,$id)
     {
-        //
+        DB::table('categories')->where('id','=',$id)->delete();
+        return redirect()->route('admin_category');
     }
 }
---
+++ resources/views/admin/category_add.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.admin')
+
+@section('title', 'Add Category')
+
+@section('content')
+<div class="content">
+    <div class="content-wrapper">
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h3>Add Category</h3>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Add Category</li>
+                        </ol>
+                    </div>
+                </div><!--/.container-fluid-->
+            </div>
+        </section>
+<!-- Main Content -->
+        <section class="content">
+
+            <!-- Default box -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Add Category</h3>
+
+            </div>
+            <div class="card-body">
+                    <!-- form start -->
+                    <form role="form" action="{{route('admin_category_create')}}" method="post">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Parent</label>
+
+                                <select class="form-control" name="parent_id" style="width: 100%;">
+                                    <option value="0" selected="selected">Main Category</option>
+
+                                @foreach($datalist as $rs)
+                                    <option value="{{$rs->id}}">{{$rs->title}}</option>
+                                @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label >Title</label>
+                                <input type="text" name="title" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label >Keywords</label>
+                                <input type="text" name="keywords" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label >Description</label>
+                                <input type="text" name="description" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Slug</label>
+                                <input type="text" name="slug" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Status</label>
+                                <select class="form-control" name="status" style="width: 100%;">
+                                    <option selected="selected">False</option>
+                                    <option>True</option>
+                                </select>
+                            </div>
+
+
+                        </div>
+
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Add Category</button>
+                        </div>
+                    </form>
+
+
+
+            <!--/.card-body -->
+                <div class="card-footer">
+
+                </div>
+                <!--/.card-footer-->
+
+                <!--/.card-->
+
+        </section>
+        <!--/.content -->
+    </div>
+    <!-- /.content-wrapper-->
+
+
+</div>
+@endsection
